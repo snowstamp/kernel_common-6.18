22d323a4d1cad94d7b456383b0ff030d4734d550
Daniel Micay <danielmicay@gmail.com>
2017-05-11 16:02:49 -0400

    randomize lower bits of the argument block
    This was based on the PaX RANDUSTACK feature in grsecurity, where all of
    the lower bits are randomized. PaX keeps 16-byte alignment.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    [levente@leventepolyak.net: do not randomize with ADDR_NO_RANDOMIZE personality]
    [levente@leventepolyak.net: adjust for mm: abstract initial stack setup to mm subsystem]
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>
    [nicolas.bouchinet@oss.cyber.gouv.fr: mm initialisation has moved to mm/vma_exec.c]
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@oss.cyber.gouv.fr>

--- mm/vma_exec.c
+++ mm/vma_exec.c
@@ -7,6 +7,7 @@
 
 #include "vma_internal.h"
 #include "vma.h"
+#include <linux/random.h>
 
 #include <linux/page_size_compat.h>
 
@@ -150,6 +151,8 @@ int create_init_stack_vma(struct mm_struct *mm, struct vm_area_struct **vmap,
 	mmap_write_unlock(mm);
 	*vmap = vma;
 	*top_mem_p = vma->vm_end - sizeof(void *);
+	if (!(current->personality & ADDR_NO_RANDOMIZE) && randomize_va_space)
+		*top_mem_p ^= get_random_u32() & ~PAGE_MASK;
 	return 0;
 
 err:
